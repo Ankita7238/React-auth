295451c575868220c8bca5ae559db579610b41e8
ANKITA <anki2gmail.com>
2024-06-15 17:33:59 +0530

    protectedRoute-authPersistence

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Redirect } from 'react-router-dom';
 
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import { useContext } from 'react';
+import AuthContext from './store/auth-context';
 
 function App() {
+  const authctx=useContext(AuthContext)
   return (
     <Layout>
       <Routes>
         <Route path='/' element={<HomePage />}/>
         <Route path='/auth' element={<AuthPage />}/>
-        <Route path='/profile' element={<UserProfile />}/>
+        <Route path='/profile' element={authctx.isLoggedIn ? <UserProfile /> : <AuthPage/>}/>
+        <Route path='*' element={<HomePage/>}/> 
       </Routes>
     </Layout>
   );
